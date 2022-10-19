b1d81508e599507e8bada550fac3c14c4160030f
luispastor <Luis.Pastor@alu.uclm.es>
2022-10-19 16:30:39 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// Typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Computer Science Engineering", "AWS Certified Cloud Practitioner"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    
+    
+    /**
+ * 
+ */
