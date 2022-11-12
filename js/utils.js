01422165cc6d6a17b6627ca0f8d559a690579d18
luispastor <Luis.Pastor@alu.uclm.es>
2022-11-12 01:01:31 +0100

    favicon and md breakpoint

---
+++ js/utils.js
@@ -0,0 +1,22 @@
+// Typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Computer Science Engineering", "AWS Certified Cloud Practitioner"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    
+    function updateCounter(){
+    	let url = "https://nkcrre35j9.execute-api.us-east-1.amazonaws.com/dev/counter"
+    	  fetch(url)
+    	    .then(response => response.json())
+    	    .then(data=> {
+    	    	let element = document.getElementById("counter")
+    	    	element.innerHTML = `${data.body.visits}`
+    	    	console.log(data); 
+    	    })
+    	    .catch(function(error) {
+    	      console.log(error); 
+    	    });  
+    	  }
+    
